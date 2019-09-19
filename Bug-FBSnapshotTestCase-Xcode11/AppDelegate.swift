a14bb69a3d6e67b2a6366f4cb8c2dec2099fcdf7
Andrew Mroczkowski <amrox@users.noreply.github.com>
2019-09-19 15:13:11 -0400

    initial commit

---
+++ Bug-FBSnapshotTestCase-Xcode11/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Bug-FBSnapshotTestCase-Xcode11
+//
+//  Created by Andrew Mroczkowski on 9/19/19.
+//  Copyright © 2019 Andy Mroczkowski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Bug-FBSnapshotTestCase-Xcode11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bug-FBSnapshotTestCase-Xcode11
+//
+//  Created by Andrew Mroczkowski on 9/19/19.
+//  Copyright © 2019 Andy Mroczkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
